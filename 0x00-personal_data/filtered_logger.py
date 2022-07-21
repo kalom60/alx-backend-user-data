4cd6cbe3fd117175e985873cdfd3f9cc57599d5f
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-21 20:26:14 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,16 +2,36 @@
 """script that hide some parts of a string"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
         fields: List[str],
         redaction: str,
         message: str,
-        separator: str):
+        separator: str) -> str:
     """return the a string with hiddening password and DOB"""
     for field in fields:
         pattern = re.search(field + "=(.*)" + separator, message).group(1)
         new_pattern = pattern.split(";")[0]
         message = re.sub(new_pattern, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initialize the class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """method to format logRecord"""
+        formatter = super(RedactingFormatter, self).format(record)
+        msg = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return msg
