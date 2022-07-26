14a2f8057638dd47a6bac4155f07bf786da71bf5
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-27 02:06:34 +0300

    add return method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,4 +58,5 @@ class BasicAuth(Auth):
                 if user.email == user_email:
                     if user.is_valid_password(user_pwd):
                         return user
+            return None
         return None
