89d0113475c461c162a2e01bd78c5761698f5a19
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-29 01:05:54 +0300

    use identifier to store return value

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""session expiration module"""
+"""session expiration"""
 from api.v1.auth.session_auth import SessionAuth
 from os import getenv
 from datetime import datetime, timedelta
@@ -16,9 +16,9 @@ class SessionExpAuth(SessionAuth):
             self.session_duration = 0
 
     def create_session(self, user_id=None):
-        """create a session"""
+        """create a session for given user id"""
         sess_id = super().create_session(user_id)
-        if isinstance(sess_id, str):
+        if sess_id not None:
             self.user_id_by_session_id[sess_id] = {
                     'user_id': user_id,
                     'created_at': datetime.now()
@@ -28,17 +28,17 @@ class SessionExpAuth(SessionAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """gets user id based on session id"""
-        if session_id is None or\
-                self.user_id_for_session_id.get(session_id, None) is None:
+        if session_id is None:
             return None
         session_obj = self.user_id_by_session_id[session_id]
+        if session_obj is None:
+            return None
         if self.session_duration <= 0:
             return session_obj['user_id']
         if 'created_at' not in session_obj:
             return None
-        current_time = datetime.time()
-        time_span = timedelta(seconds=self.session_duration)
-        exp_time = session_obj['created_at'] + time_span
-        if exp_time < current_time:
+        session_span = timedelta(seconds=self.session_duration)
+        exp_time = session_obj['created_at'] + session_span
+        if exp_time < datetime.now():
             return None
         return session_obj['user_id']
