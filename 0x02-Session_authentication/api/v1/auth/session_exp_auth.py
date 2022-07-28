8eb9a9c79987793b784acd976bced11a1699a3bd
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-29 01:07:25 +0300

    docstring

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""session expiration"""
+"""session authentication expiration"""
 from api.v1.auth.session_auth import SessionAuth
 from os import getenv
 from datetime import datetime, timedelta
