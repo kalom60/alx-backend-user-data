cfe27e87f2aafed5923f9b0c8a4fd27f5da82fcc
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-29 01:31:13 +0300

    Sessions in database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,15 +17,18 @@ auth = None
 if getenv("AUTH_TYPE") == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-if getenv("AUTH_TYPE") == 'basic_auth':
+elif getenv("AUTH_TYPE") == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-if getenv("AUTH_TYPE") == "session_auth":
+elif getenv("AUTH_TYPE") == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-if getenv("AUTH_TYPE") == "session_exp_auth":
+elif getenv("AUTH_TYPE") == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+elif getenv("AUTH_TYPE") == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
