79a35fcdb65f11bd1abbd1cf6e2f6ea57949adb7
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-28 23:21:46 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -50,11 +50,13 @@ def before_request():
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
             ]
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None or\
+                    auth.session_cookie(request):
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
