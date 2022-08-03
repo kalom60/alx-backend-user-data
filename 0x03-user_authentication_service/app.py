7f92d193eb1657241ab4bcf8f853181aec9a059c
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-03 14:18:28 +0300

    fix syntax error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,12 +46,12 @@ def login() -> str:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> str:
+def logout():
     """
     DELETE /sessions
     logout and destroy session
     """
-    session_id = request.cookie.get('session_id')
+    session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
