18444654cb43a0bbfa8914ed1607a6487dc91e12
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-03 14:24:10 +0300

    user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,5 +59,18 @@ def logout():
     abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """
+    GET /profile
+    returns user email
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id('session_id')
+    if user:
+        return jsonify({"email": f'{user.email}'}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
