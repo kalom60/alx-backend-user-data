504984ba40847eaf694e1431cad8289459102bfd
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-04 17:51:00 +0300

    remove parameter annonation for update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,7 +54,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """method to update attributes of a user"""
         user = self.find_user_by(id=user_id)
         if user:
