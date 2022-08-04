c6bcbb486a30fcb4f878cf3b21215ba574cbcaae
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-04 17:25:11 +0300

    check if kwargs is empty

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,16 +40,16 @@ class DB:
 
     def find_user_by(self, **kwargs: dict) -> User:
         """method to query a user based on a given input"""
-        if kwargs:
-            keys = User.__table__.columns.keys()
-            for key in kwargs.keys():
-                if key not in keys:
-                    raise InvalidRequestError()
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound()
-            return user
-        raise InvalidRequestError()
+        if not kwargs:
+            raise InvalidRequestError
+        keys = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in keys:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
 
     def update_user(self, user_id: int, **kwargs: dict) -> None:
         """method to update attributes of a user"""
